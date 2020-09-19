721c02f448892f99210f9b5020177a7565f4d25c
DeShawne <deshawne@DeShawnes-MBP.attlocal.net>
2020-09-19 09:20:25 -0400

    finished

--- pokepi.js
+++ pokepi.js
@@ -1,24 +1,21 @@
-const baseUrl = 'https://pokeapi.co/api/v2/pokemon?limit=892&offset=200'
+const baseUrl = 'https://pokeapi.co/api/v2/'
 let url;
+
 const searchTerm = document.querySelector('.search');
 const searchForm = document.querySelector('form');
 const submitBtn = document.querySelector('.submit');
-const nextBtn = document.querySelector('.next');
-const previousBtn = document.querySelector('.prev');
 const nav = document.querySelector('nav');
 const section = document.querySelector('section');
-nav.style.display = 'none';
-let pageNumber = 0;
 
 
 searchForm.addEventListener('submit', fetchResults);
-nextBtn.addEventListener('click', nextPage);
-previousBtn.addEventListener('click', previousPage);
+
 
 function fetchResults(e) {
      //console.log(e);
     e.preventDefault();
-    url = `${baseUrl}&page=${pageNumber}&q=${searchTerm.value}`;
+
+    url = `${baseUrl}pokemon/${searchTerm.value}`;
     
     fetch(url)
     .then(function (result) {
@@ -33,41 +30,39 @@ function fetchResults(e) {
 
 function displayResults(json) {
     console.log('Display Results', json);
-    //console.log(json.response.docs);
     while (section.firstChild) {
         section.removeChild(section.firstChild);
-    }
+    } 
 
+    let card = json; // is card correct? Should this be an object/property from json data?
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-function nextPage(e) {
-    // console.log('Next button clicked');
-    pageNumber++;
-    fetchResults(e);
-    console.log('Page Number:', pageNumber);
-}
-
-function previousPage(e) {
-    // console.log('Previous button clicked');
-    if (pageNumber > 0) {
-        pageNumber--;
+    if (card.length === 0) {
+        console.log('no results');
     } else {
-        return;
+        // for (i = 0, i < card.length, i++) {
+            const avitar = document.createElement('img');
+            const name = document.createElement('h1');
+            const height = document.createElement('h4');
+            const weight = document.createElement('h4');
+        
+            avitar.src = card.sprites.front_default;
+            avitar.alt = card.name;
+            name.className = 'pokémonName';
+            name.innerText = card.name;
+            name.style = 'font-family: Trebuchet MS, Lucida Sans Unicode, Lucida Grande, Lucida Sans, Arial, sans-serif; color: black;';
+               
+            height.className = 'pokémonHeight';
+            height.innerText = `${card.height} inches.`;
+            height.style = 'font-family: Trebuchet MS, Lucida Sans Unicode, Lucida Grande, Lucida Sans, Arial, sans-serif; color: #f7f307;';
+            weight.className = 'pokémonWeight';
+            weight.innerText = `${card.weight} pounds`;
+            weight.style = 'font-family: Trebuchet MS, Lucida Sans Unicode, Lucida Grande, Lucida Sans, Arial, sans-serif; color: red;';
+
+            section.appendChild(avitar);
+            section.appendChild(name);
+            section.appendChild(height);
+            section.appendChild(weight);
+            fetch(url);
+        }
     }
-    fetchResults(e);
-    console.log('Page:', pageNumber);
-}
+//}
